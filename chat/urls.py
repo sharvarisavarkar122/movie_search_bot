6a0be99e93eeb8c8f77da268928a17b47f1c203e
Sharvari Savarkar <166944634+sharvarisavarkar122@users.noreply.github.com>
2024-05-20 13:11:35 +0530

    Add files via upload

---
+++ chat/urls.py
@@ -0,0 +1,8 @@
+# chat/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('movie_search/', views.movie_search, name='movie_search'),
+]
---
+++ movie_search_bot/settings_rail.py
@@ -0,0 +1,3 @@
+from distutils import config # type: ignore
+from movie_search_bot.settings import *
+SECRET_KEY=config('SECRET_KEY')
